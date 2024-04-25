bbaeda80fa9df4a717c99fffa713ef16b6658427
Colin Leung <colin@cowise.co>
2024-04-26 05:09:28 +0800

    fix: import modules

--- init.mjs
+++ init.mjs
@@ -1,6 +1,6 @@
 import { Central } from '@lionrockjs/central';
 
 Central.initConfig(new Map([
-  ['auth', require('./config/auth')],
-  ['register', require('./config/register')],
+  ['auth', await import('./config/auth.mjs')],
+  ['register', await import('./config/register.mjs')],
 ]));
