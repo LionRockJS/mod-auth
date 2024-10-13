0c2067ee610889986fc86155d5145e8f58eac2af
Colin Leung <colin@cowise.co>
2024-10-13 11:38:24 +0800

    feat: v2 filename and configs

--- init.mjs
+++ init.mjs
@@ -1,6 +1,1 @@
-import { Central } from '@lionrockjs/central';
-
-await Central.initConfig(new Map([
-  ['auth', await import('./config/auth.mjs')],
-  ['register', await import('./config/register.mjs')],
-]));
+export default {}
